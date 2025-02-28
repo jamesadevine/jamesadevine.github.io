bff457ad57fd29cab0abe26b1d64938d4aaa0b08
James Devine <james_a_devine@outlook.com>
2025-02-28 23:28:51 +0000

    WIP?

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,15 @@
-import { defineCollection } from 'astro:content';
+import { z, defineCollection } from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
+    docs: defineCollection({ schema: docsSchema() }),
+    blog: defineCollection({
+        schema: z.object({
+            title: z.string(),
+            description: z.string(),
+            tags: z.array(z.string()),
+            date: z.string(),
+            image: z.string().optional(),
+        })
+    }),
 };

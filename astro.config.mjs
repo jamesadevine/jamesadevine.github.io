0f33cda8ec68a05ca1cddc37783a4c5fc72cdf06
James Devine <james_a_devine@outlook.com>
2025-07-27 22:51:48 +0100

    update gitsha propagation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import { execSync } from 'child_process';
+
+const gitSha = execSync('git rev-parse --short HEAD').toString().trim();
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://jamesadevine.com',
 	integrations: [mdx(), sitemap()],
+	vite: {
+		define: {
+			__GIT_SHA__: JSON.stringify(gitSha),
+		},
+  	},
 });
--- src/utils/gitSha.ts
+++
@@ -1,7 +0,0 @@
-import { execSync } from "child_process";
-
-export const GET_GIT_SHA = () => {
-    return execSync("git rev-parse --short HEAD")
-        .toString()
-        .trim()
-}

d35ed6ccf6f31e5472fabba0b0a5f764b7933f08
James Devine <james_a_devine@outlook.com>
2025-08-25 14:36:23 +0100

    fixup gitsha

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,15 @@ import { execSync } from 'child_process';
 
 const gitSha = execSync('git rev-parse --short HEAD').toString().trim();
 
+console.log(`Building site with git SHA: ${gitSha}`);
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://jamesadevine.com',
 	integrations: [mdx(), sitemap()],
 	vite: {
 		define: {
-			__GIT_SHA__: JSON.stringify(gitSha),
+			'import.meta.env.GIT_SHA': JSON.stringify(gitSha),
 		},
   	},
 });
